bac8d918dbf13ed2cdea04ddaf50336b813789e3
qinyafei <13941663749@163.com>
2020-05-08 21:53:11 +0800

    整理项目

--- LightMVC/assets/script/Notification.ts
+++
@@ -1,6 +0,0 @@
-
-export default class Notification {
-
-    static readonly UPDATE_EXP_FINISH = "UPDATE_EXP_FINISH";
-    
-}
--- LightMVC/assets/script/view/SecondScene.ts
+++
@@ -1,9 +0,0 @@
-import BaseScene from "../../libs/lightMVC/core/base/BaseScene";
-
-const {ccclass, property} = cc._decorator;
-
-@ccclass
-export default class SecondScene extends BaseScene {
-
-   
-}
